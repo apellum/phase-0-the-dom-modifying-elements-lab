029e52f156772a73f05c281e6c596d02939bdad5
Andrew Pellum <apellum19@gmail.com>
2021-05-29 13:00:02 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector(`main`).remove();
+
+let newHeader = document.createElement(`h1`);
+
+newHeader.id = `victory`;
+
+newHeader.innerHTML = "<h1>Drew is the champion</h1>"
+
+document.body.append(newHeader)
