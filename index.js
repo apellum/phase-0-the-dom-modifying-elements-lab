0e4614209eb0c1f6c550767ba116fa43744da4a7
Andrew Pellum <apellum19@gmail.com>
2021-06-15 13:41:56 -0600

    commit tree 8ef34d831b99477723e0775db720198876cfe8b5

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 // Write your code here!
-document.querySelector(`main`).remove();
+document.querySelector('main#main').remove();
 
-let newHeader = document.createElement(`h1`);
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
 
-newHeader.id = `victory`;
+document.body.appendChild(newHeader)
 
-newHeader.innerHTML = "<h1>Drew is the champion</h1>"
+newHeader.innerHTML = '<h1>DREW is the champion</h1>';
 
-document.body.append(newHeader)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.querySelector(`main`).remove();
+//
+//let newHeader = document.createElement(`h1`);
+//
+//newHeader.id = `victory`;
+//
+//newHeader.innerHTML = "<h1>Drew is the champion</h1>"
+//
+//document.body.append(newHeader)
